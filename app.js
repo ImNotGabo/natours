2767de28e8548f66155714a35b03ba375b77ec62
ImNotGabo <gabriel.duarte18@outlook.es>
2024-12-21 18:39:06 -0400

    Add DELETE endpoint for removing tours

--- app.js
+++ app.js
@@ -97,6 +97,20 @@ app.patch('/api/v1/tours/:id', (req, res) => {
 	});
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+	if (id > tours.length) {
+		return res.status(404).json({
+			status: 'fail',
+			message: 'Invalid ID',
+		});
+	}
+	res.status(204).json({
+		status: 'success',
+		data: null,
+	});
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
 	console.log(`App runinng on ${port}...`);
