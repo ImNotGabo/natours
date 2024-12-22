1107c07e93cf6e77fe046d9f952d2fda2356dd6f
ImNotGabo <gabriel.duarte18@outlook.es>
2024-12-22 19:21:11 -0400

    Refactor app.js to modularize routes for tours and users

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const fs = require('node:fs');
-const { writeFile } = require('node:fs/promises');
 const express = require('express');
 const morgan = require('morgan');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
@@ -20,146 +20,9 @@ app.use((req, res, next) => {
 	next();
 });
 
-const tours = JSON.parse(
-	fs.readFileSync(`${__dirname}/dev-data/data/tour-simple.json`)
-);
-
 // Route handles
 
-const getAllTours = (req, res) => {
-	res.status(200).json({
-		status: 'success',
-		requestedAt: req.requestTime,
-		results: tours.length,
-		data: { tours },
-	});
-};
-
-const getTour = (req, res) => {
-	// console.log(req.params);
-	const id = parseInt(req.params.id);
-	if (id > tours.length) {
-		return res.status(404).json({
-			status: 'fail',
-			message: 'Invalid ID',
-		});
-	}
-	const tour = tours.find((ele) => ele.id === id);
-	res.status(200).json({
-		status: 'success',
-		data: { tour },
-	});
-};
-
-const createTour = async (req, res) => {
-	try {
-		// Basic body validation
-		if (!req.body) {
-			return res.status(400).json({
-				status: 'fail',
-				message: 'No data provided',
-			});
-		}
-
-		const newId = tours.length > 0 ? tours[tours.length - 1].id + 1 : 1;
-		const newTour = { id: newId, ...req.body };
-
-		tours.push(newTour);
-
-		await writeFile(
-			`${__dirname}/dev-data/data/tour-simple.json`,
-			JSON.stringify(tours, null, 2) //JSON formant
-		);
-
-		return res.status(201).json({
-			status: 'success',
-			data: {
-				tour: newTour,
-			},
-		});
-	} catch (error) {
-		return res.status(500).json({
-			status: 'error',
-			message: 'Error to save tour',
-			error: process.env.NODE_ENV === 'development' ? error.message : undefined,
-		});
-	}
-};
-
-const updateTour = (req, res) => {
-	const id = parseInt(req.params.id);
-	if (id > tours.length) {
-		return res.status(404).json({
-			status: 'fail',
-			message: 'Invalid ID',
-		});
-	}
-	res.status(200).json({
-		status: 'success',
-		data: {
-			tour: '<Updated tour here...>',
-		},
-	});
-};
-
-const deleteTour = (req, res) => {
-	const id = parseInt(req.params.id);
-	if (id > tours.length) {
-		return res.status(404).json({
-			status: 'fail',
-			message: 'Invalid ID',
-		});
-	}
-	res.status(204).json({
-		status: 'success',
-		data: null,
-	});
-};
-
-const getAllUsers = (req, res) => {
-	res.status(500).json({
-		status: 'error',
-		message: 'This route is not yet defined!',
-	});
-};
-
-const createUser = (req, res) => {
-	res.status(500).json({
-		status: 'error',
-		message: 'This route is not yet defined!',
-	});
-};
-
-const getUser = (req, res) => {
-	res.status(500).json({
-		status: 'error',
-		message: 'This route is not yet defined!',
-	});
-};
-const updateUser = (req, res) => {
-	res.status(500).json({
-		status: 'error',
-		message: 'This route is not yet defined!',
-	});
-};
-const deleteUser = (req, res) => {
-	res.status(500).json({
-		status: 'error',
-		message: 'This route is not yet defined!',
-	});
-};
-
 // Routes
-const tourRouter = express.Router();
-const userRouter = express.Router();
-
-tourRouter.route('/').get(getAllTours).post(createTour);
-
-tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
-
-userRouter.route('/').get(getAllUsers).post(createUser);
-
-userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
---
+++ controllers/tourController.js
@@ -0,0 +1,96 @@
+const fs = require('node:fs');
+const { writeFile } = require('node:fs/promises');
+
+const tours = JSON.parse(
+	fs.readFileSync(`${__dirname}/../dev-data/data/tour-simple.json`)
+);
+
+exports.getAllTours = (req, res) => {
+	res.status(200).json({
+		status: 'success',
+		requestedAt: req.requestTime,
+		results: tours.length,
+		data: { tours },
+	});
+};
+
+exports.getTour = (req, res) => {
+	// console.log(req.params);
+	const id = parseInt(req.params.id);
+	if (id > tours.length) {
+		return res.status(404).json({
+			status: 'fail',
+			message: 'Invalid ID',
+		});
+	}
+	const tour = tours.find((ele) => ele.id === id);
+	res.status(200).json({
+		status: 'success',
+		data: { tour },
+	});
+};
+
+exports.createTour = async (req, res) => {
+	try {
+		// Basic body validation
+		if (!req.body) {
+			return res.status(400).json({
+				status: 'fail',
+				message: 'No data provided',
+			});
+		}
+
+		const newId = tours.length > 0 ? tours[tours.length - 1].id + 1 : 1;
+		const newTour = { id: newId, ...req.body };
+
+		tours.push(newTour);
+
+		await writeFile(
+			`${__dirname}/dev-data/data/tour-simple.json`,
+			JSON.stringify(tours, null, 2) //JSON formant
+		);
+
+		return res.status(201).json({
+			status: 'success',
+			data: {
+				tour: newTour,
+			},
+		});
+	} catch (error) {
+		return res.status(500).json({
+			status: 'error',
+			message: 'Error to save tour',
+			error: process.env.NODE_ENV === 'development' ? error.message : undefined,
+		});
+	}
+};
+
+exports.updateTour = (req, res) => {
+	const id = parseInt(req.params.id);
+	if (id > tours.length) {
+		return res.status(404).json({
+			status: 'fail',
+			message: 'Invalid ID',
+		});
+	}
+	res.status(200).json({
+		status: 'success',
+		data: {
+			tour: '<Updated tour here...>',
+		},
+	});
+};
+
+exports.deleteTour = (req, res) => {
+	const id = parseInt(req.params.id);
+	if (id > tours.length) {
+		return res.status(404).json({
+			status: 'fail',
+			message: 'Invalid ID',
+		});
+	}
+	res.status(204).json({
+		status: 'success',
+		data: null,
+	});
+};
