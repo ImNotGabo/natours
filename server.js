3293155350d904fefe0ba0bfed0553823dd66893
ImNotGabo <gabriel.duarte18@outlook.es>
2024-12-23 22:56:32 -0400

    Add dotenv configuration

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const dotenv = require('dotenv').config({ path: './.env' });
 const app = require('./app');
 
 // Start the server
+// console.log(process.env);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
