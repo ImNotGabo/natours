33c2b97f938e3704c47491b4e1f3bbeb8fd96d30
ImNotGabo <gabriel.duarte18@outlook.es>
2024-12-28 10:38:00 -0400

    Add tour schema and model to server.js for tour management

--- server.js
+++ server.js
@@ -10,8 +10,26 @@ const pwd = process.env.DB_PASSWORD;
 const DB = process.env.DB_HOST.replace(/USER:PWD/, `${user}:${pwd}`);
 mongoose.connect(DB).then(console.log('Connected!'));
 
+const tourSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, 'A tour must have a name'],
+		unique: true,
+	},
+	rating: {
+		type: Number,
+		default: 4.5,
+	},
+	price: {
+		type: Number,
+		required: [true, 'A tour must have a price'],
+	},
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, (con) => {
+app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
