532c5344ad3f72766590158958e9b7e899fee6ae
ImNotGabo <gabriel.duarte18@outlook.es>
2024-12-28 12:22:25 -0400

    Refactor tourController.js to remove file system operations and integrate Tour model

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,23 +1,4 @@
-import { readFileSync } from 'node:fs';
-import { writeFile } from 'node:fs/promises';
-import { getDirname } from '../utils/utils.js';
-
-const __dirname = getDirname(import.meta.url);
-
-const tours = JSON.parse(
-	readFileSync(`${__dirname}/../dev-data/data/tour-simple.json`)
-);
-
-export function checkID(req, res, next, value) {
-	const id = parseInt(req.params.id);
-	if (id > tours.length) {
-		return res.status(404).json({
-			status: 'fail',
-			message: 'Invalid ID',
-		});
-	}
-	next();
-}
+import Tour from '../models/toursModel.js';
 
 export function checkBody(req, res, next) {
 	const { name, price } = req.body;
@@ -34,54 +15,21 @@ export function getAllTours(req, res) {
 	res.status(200).json({
 		status: 'success',
 		requestedAt: req.requestTime,
-		results: tours.length,
-		data: { tours },
+		/* results: tours.length,
+		data: { tours }, */
 	});
 }
 
 export function getTour(req, res) {
 	const id = parseInt(req.params.id);
-	const tour = tours.find((ele) => ele.id === id);
+	/* const tour = tours.find((ele) => ele.id === id);
 	res.status(200).json({
 		status: 'success',
 		data: { tour },
-	});
+	}); */
 }
 
-export async function createTour(req, res) {
-	try {
-		// Basic body validation
-		if (!req.body) {
-			return res.status(400).json({
-				status: 'fail',
-				message: 'No data provided',
-			});
-		}
-		const newTour = await writeFile(
-			`${__dirname}/../dev-data/data/tour-simple.json`,
-			JSON.stringify(tours, null, 2) // JSON format
-		);
-		tours.push(newTour);
-
-		await writeFile(
-			`${__dirname}/../dev-data/data/tour-simple.json`,
-			JSON.stringify(tours, null, 2) //JSON formant
-		);
-
-		return res.status(201).json({
-			status: 'success',
-			data: {
-				tour: newTour,
-			},
-		});
-	} catch (error) {
-		return res.status(500).json({
-			status: 'error',
-			message: 'Error to save tour',
-			error: process.env.NODE_ENV === 'development' ? error.message : undefined,
-		});
-	}
-}
+export async function createTour(req, res) {}
 
 export function updateTour(req, res) {
 	res.status(200).json({
