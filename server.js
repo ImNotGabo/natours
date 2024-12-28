829a465298cde7c96c252f15dc245bc5f7710b61
ImNotGabo <gabriel.duarte18@outlook.es>
2024-12-28 10:32:20 -0400

    Add mongoose for database connection and refactor server.js to use dotenv for environment variables

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-import 'dotenv/config';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import app from './app.js';
 
+dotenv.config();
+
+const user = process.env.DB_USER;
+const pwd = process.env.DB_PASSWORD;
+
+const DB = process.env.DB_HOST.replace(/USER:PWD/, `${user}:${pwd}`);
+mongoose.connect(DB).then(console.log('Connected!'));
+
 // Start the server
-// console.log(process.env);
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, (con) => {
 	console.log(`Server is running on port ${PORT}`);
 });
