4e2ae23364164abdd566aba53754d4adfff59c65
ImNotGabo <gabriel.duarte18@outlook.es>
2024-12-28 12:22:18 -0400

    Remove tour schema and test data from server.js

--- server/server.js
+++ server/server.js
@@ -10,34 +10,6 @@ const pwd = process.env.DB_PASSWORD;
 const DB = process.env.DB_HOST.replace(/USER:PWD/, `${user}:${pwd}`);
 mongoose.connect(DB).then(console.log('Connected!'));
 
-const tourSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: [true, 'A tour must have a name'],
-		unique: true,
-	},
-	rating: {
-		type: Number,
-		default: 4.5,
-	},
-	price: {
-		type: Number,
-		required: [true, 'A tour must have a price'],
-	},
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-	name: 'The Park Camper',
-	price: 997,
-});
-
-testTour
-	.save()
-	.then((doc) => console.log(doc))
-	.catch((err) => console.error(err));
-
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
