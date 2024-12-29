d4321bd5e8a7eac819bb5b28ae3ba9ba5d46b20a
ImNotGabo <gabriel.duarte18@outlook.es>
2024-12-29 12:59:47 -0400

    Implement async deleteTour function with error handling and response structure

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -71,9 +71,17 @@ export async function updateTour(req, res) {
 	}
 }
 
-export function deleteTour(req, res) {
-	res.status(204).json({
-		status: 'success',
-		data: null,
-	});
+export async function deleteTour(req, res) {
+	try {
+		const deleteTour = await Tour.findByIdAndDelete(req.params.id);
+		res.status(204).json({
+			status: 'success',
+			data: deleteTour,
+		});
+	} catch (error) {
+		res.status(400).json({
+			status: 'fail',
+			message: error,
+		});
+	}
 }
