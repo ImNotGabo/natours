faeded17b701b02e247b46425049a6222ffa267c
ImNotGabo <gabriel.duarte18@outlook.es>
2025-01-03 12:14:17 -0400

    Add blacklisted queries

--- utils/utils.js
+++ utils/utils.js
@@ -3,10 +3,11 @@ import path from 'path';
 import dotenv from 'dotenv';
 dotenv.config();
 
+export const BLACKLISTED_QUERIES = ['page', 'sort', 'limit', 'fields'];
+export const USER = process.env.DB_USER;
+export const PWD = process.env.DB_PASSWORD;
+
 export const getDirname = (metaUrl) => {
 	const __filename = fileURLToPath(metaUrl);
 	return path.dirname(__filename);
 };
-
-export const USER = process.env.DB_USER;
-export const PWD = process.env.DB_PASSWORD;
