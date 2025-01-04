79e170e9911cda7507b0753a4a4094b2204da118
ImNotGabo <gabriel.duarte18@outlook.es>
2025-01-04 19:47:01 -0400

    Add pagination utility function for handling page and limit parameters

--- utils/utils.js
+++ utils/utils.js
@@ -11,3 +11,10 @@ export const getDirname = (metaUrl) => {
 	const __filename = fileURLToPath(metaUrl);
 	return path.dirname(__filename);
 };
+
+export const pagination = (req) => {
+	const PAGE = parseInt(req.query.page, 10) || 1;
+	const LIMIT = parseInt(req.query.limit, 10) || 100;
+	const SKIP = (PAGE - 1) * LIMIT;
+	return { PAGE, LIMIT, SKIP };
+};
