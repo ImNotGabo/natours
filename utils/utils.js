810f473817dd53e5d71c4824d5f5af42e3819607
ImNotGabo <gabriel.duarte18@outlook.es>
2025-01-05 15:27:36 -0400

    Remove unused utility functions for directory retrieval and pagination

--- utils/utils.js
+++ utils/utils.js
@@ -1,20 +1,6 @@
-import { fileURLToPath } from 'url';
-import path from 'path';
 import dotenv from 'dotenv';
 dotenv.config();
 
 export const BLACKLISTED_QUERIES = ['page', 'sort', 'limit', 'fields'];
 export const USER = process.env.DB_USER;
 export const PWD = process.env.DB_PASSWORD;
-
-export const getDirname = (metaUrl) => {
-	const __filename = fileURLToPath(metaUrl);
-	return path.dirname(__filename);
-};
-
-export const pagination = (req) => {
-	const PAGE = parseInt(req.query.page, 10) || 1;
-	const LIMIT = parseInt(req.query.limit, 10) || 100;
-	const SKIP = (PAGE - 1) * LIMIT;
-	return { PAGE, LIMIT, SKIP };
-};
