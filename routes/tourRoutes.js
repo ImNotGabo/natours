6546e20affa0a75403f1160d736830554f040195
ImNotGabo <gabriel.duarte18@outlook.es>
2025-01-05 23:57:19 -0400

    Add route for tour statistics

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,6 +3,7 @@ import {
 	aliasTopTours,
 	getAllTours,
 	getTour,
+	getTourStats,
 	createTour,
 	updateTour,
 	deleteTour,
@@ -12,6 +13,7 @@ const router = Router();
 
 // router.param('id', checkID);
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+router.route('/tour-stats').get(getTourStats);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
