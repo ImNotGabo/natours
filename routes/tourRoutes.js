668a69250958217c7f8827c77b53821115f978b4
ImNotGabo <gabriel.duarte18@outlook.es>
2025-01-06 01:09:35 -0400

    Add route for monthly tour plan

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,6 +4,7 @@ import {
 	getAllTours,
 	getTour,
 	getTourStats,
+	getMonthlyPlan,
 	createTour,
 	updateTour,
 	deleteTour,
@@ -14,6 +15,7 @@ const router = Router();
 // router.param('id', checkID);
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
